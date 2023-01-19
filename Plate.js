5754315dc03f8dd05e6917b56047e597fdd6e248
Robert Gicheha <122348430+robertgicheha@users.noreply.github.com>
2023-01-19 09:42:01 +0300

    number plate challenge

---
+++ Plate.js
@@ -0,0 +1,22 @@
+function countPlates(plate1, plate2) {n
+    var count = 0;
+    var prefix1 = plate1.substring(0, 3);
+    var prefix2 = plate2.substring(0, 3);
+    var suffix1 = plate1.substring(4);
+    var suffix2 = plate2.substring(4);
+    if (prefix1 === prefix2) {
+        count = Math.abs(suffix2 - suffix1) + 1;
+    } else {
+        count = ((999 - suffix1) + 1) + (suffix2 - 1);
+    }
+    console.log("Number of plates registered between " + plate1 + " and " + plate2 + ": " + count);
+   
+}
+
+/You can test the function like this/
+
+// countPlates("KAA-0001", "KAB-0005");
+
+// The function will output "Number of plates registered between KAA-0001 and KAA-0005: 5" and "Number of plates registered between KAA-0001 and KAB-0005: 1005" respectivelygit 
+
+// Note: This function assumes that the input license plates are always in valid format "Kxx-000x" and there is no validation of the input being passed to the function. Also this function assumes that the plates are being registered in a continuous mann
